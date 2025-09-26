78227ad2f624e298bab6f524715d2f4d56081341
Jason Jun <uxd.jun@gmail.com>
2025-09-26 16:54:09 +1000

    Add tes image data

---
+++ data/images.ts
@@ -0,0 +1,111 @@
+export interface ImageData {
+	fileName: string;
+	width: number;
+	height: number;
+	title: string;
+}
+
+export const images: ImageData[] = [
+	{
+		fileName: "00.jpg",
+		width: 5168,
+		height: 3448,
+		title: "Urban Sunset Glow",
+	},
+	{
+		fileName: "01.jpg",
+		width: 6720,
+		height: 4480,
+		title: "Mountain Vista",
+	},
+	{
+		fileName: "02.jpg",
+		width: 5464,
+		height: 8192,
+		title: "Abstract Motion",
+	},
+	{
+		fileName: "03.jpg",
+		width: 4160,
+		height: 6240,
+		title: "City Lights",
+	},
+	{
+		fileName: "04.jpg",
+		width: 2160,
+		height: 3840,
+		title: "Coastal Dreams",
+	},
+	{
+		fileName: "05.jpg",
+		width: 3000,
+		height: 3000,
+		title: "Geometric Patterns",
+	},
+	{
+		fileName: "06.jpg",
+		width: 4608,
+		height: 3072,
+		title: "Morning Mist",
+	},
+	{
+		fileName: "07.jpg",
+		width: 4545,
+		height: 6818,
+		title: "Neon Nights",
+	},
+	{
+		fileName: "08.jpg",
+		width: 4405,
+		height: 6607,
+		title: "Forest Path",
+	},
+	{
+		fileName: "09.jpg",
+		width: 6000,
+		height: 3375,
+		title: "Desert Horizon",
+	},
+	{
+		fileName: "10.jpg",
+		width: 5996,
+		height: 4003,
+		title: "Ocean Waves",
+	},
+	{
+		fileName: "11.jpg",
+		width: 2667,
+		height: 4000,
+		title: "Street Art",
+	},
+	{
+		fileName: "12.jpg",
+		width: 2667,
+		height: 4000,
+		title: "Vintage Mood",
+	},
+	{
+		fileName: "13.jpg",
+		width: 6774,
+		height: 4492,
+		title: "Golden Hour",
+	},
+	{
+		fileName: "14.jpg",
+		width: 3511,
+		height: 5266,
+		title: "Urban Architecture",
+	},
+	{
+		fileName: "15.jpg",
+		width: 8177,
+		height: 5451,
+		title: "Natural Wonders",
+	},
+	{
+		fileName: "16.jpg",
+		width: 4228,
+		height: 2819,
+		title: "Minimalist Space",
+	},
+];
